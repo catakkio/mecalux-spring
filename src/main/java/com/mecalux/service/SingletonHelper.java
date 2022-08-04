dcc92f086c6c27518601518fa0e5135d17e4bb5f
Alessandro Catacchio <a.catacchio5@gmail.com>
2022-08-04 16:15:12 +0200

    First 2 test cases implemented

--- src/main/java/com/mecalux/service/SingletonHelper.java
+++ src/main/java/com/mecalux/service/SingletonHelper.java
@@ -1,8 +1,27 @@
 package com.mecalux.service;
 
+import java.util.UUID;
+
 /**
  * TODO: 1. Implementar un singleton (com.mecalux.service.SingletonHelper) que proporcione a su vez un método que devuelva un id Random
  */
 public class SingletonHelper {
+    private static SingletonHelper instance;
+    private static int id=0;
 
+    private SingletonHelper(){}; // we made the constructor not accessible
+    public static SingletonHelper getInstance() {
+        if(instance == null) {
+            instance = new SingletonHelper();
+        }
+        return instance;
+    }
+
+    public int getIncrementalRandomId(){
+        return id++;
+    }
+
+    public String getRandomId(){
+        return UUID.randomUUID().toString();
+    }
 }
--- src/test/java/com/mecalux/service/TesteableTest.java
+++ src/test/java/com/mecalux/service/TesteableTest.java
@@ -1,9 +1,48 @@
 package com.mecalux.service;
 
+import org.junit.Test;
+import org.mockito.Mockito;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.mock;
+
 /**
  * TODO: 2. Implementa los Tests unitarios de la clase TesteableTest.java. Tienes las librerías de JUnit y Mockito en el proyecto, pero puedes usar lo que
  * consideres oportuno.
  */
 public class TesteableTest {
 
+
+    private Testeable testeable = new Testeable();
+
+
+    @Test
+    public void testMetodo1WithoutNullValues() {
+        assertEquals(testeable.metodo1("STRING_0"), "TESTED_STRING_0");
+
+        assertEquals(testeable.metodo1("test one"), "TESTED_test one");
+
+        assertEquals(testeable.metodo1("name"), "TESTED_name");
+    }
+
+    @Test
+    public void testMetodo1WithNullValues() {
+        assertEquals(testeable.metodo1(null), null);
+    }
+
+    @Test
+    public void testMetodo2(){
+        IValuable mockValuable = mock(IValuable.class);
+        Mockito.when(mockValuable.getValue()).thenReturn("MOCKED_VALUE_0");
+        assertEquals(testeable.metodo2(mockValuable), "TESTED_MOCKED_VALUE_0");
+
+        Mockito.when(mockValuable.getValue()).thenReturn("GET/VALUE&0");
+        assertEquals(testeable.metodo2(mockValuable), "TESTED_GET/VALUE&0");
+
+        Mockito.when(mockValuable.getValue()).thenReturn("");
+        assertEquals(testeable.metodo2(mockValuable), "TESTED_");
+
+        Mockito.when(mockValuable.getValue()).thenReturn(null);
+        assertEquals(testeable.metodo2(mockValuable), "TESTED_null");
+    }
 }
