4ebb208996df67d9be71828bb3d9fe2d6f17a566
Alessandro Catacchio <a.catacchio5@gmail.com>
2022-08-04 19:23:16 +0200

    Working get and post requests

--- src/main/java/com/mecalux/controller/UserController.java
+++ src/main/java/com/mecalux/controller/UserController.java
@@ -1,9 +1,36 @@
 package com.mecalux.controller;
 
+import com.mecalux.model.User;
+import org.springframework.web.bind.annotation.*;
+
 /**
  * TODO: 4.	Propón los métodos necesarios (no hace falta implementar el cuerpo del método) con Spring MVC o Jersey para montar una API REST de la entidad
  * com.mecalux.model.User en la clase UserController.java
  */
+@RestController
 public class UserController {
+    /**
+     *
+     * @param user the user to add to the DB
+     * @return return a response entity with the user passed as input parameter and the response status
+     */
+    @PostMapping("/user")
+    public User addUser(@RequestBody User user){
+        return user; //TODO implement body of the method
+    }
+
+
+//    @RequestMapping(value = "/user", method = RequestMethod.GET,params = "id")
+
+    /**
+     * Request example: localhost:8080/user/15
+     * @param id the user id
+     * @return the requested user
+     */
+    @GetMapping("/user/{id}")
+    String getUserById(@PathVariable String id) {
+        return "Retrieved user with id:"+id ; //TODO implement body of the method
+    }
+
 
 }
