426223169d58a4d5b152302ebc4190dede534846
Alessandro Catacchio <a.catacchio5@gmail.com>
2022-08-05 12:07:01 +0200

    Exercise about combinations done

--- src/main/java/ejercicio2/Main.java
+++ src/main/java/ejercicio2/Main.java
@@ -1,5 +1,9 @@
 package ejercicio2;
 
+import org.assertj.core.util.Lists;
+
+import java.util.List;
+
 /**
  * TODO 6.	Uno de nuestros clientes quiere comprar 6 estantes para su nuevo almacén. Tenemos 3 soluciones:
  *
@@ -14,6 +18,45 @@ package ejercicio2;
 public class Main {
 
     public static void main(String[] args) {
-
+        printProductCombinations();
     }
+
+    // Expected result: 3^6 = 729 records
+    // I thought we were talking about combinations more than permutations because we have 6 spaces to fill.
+    // We would have had a permutation if we had 3 spaces to fill
+
+    /**
+     *
+     *  I thought we were talking about combinations more than permutations because we have 6 spaces to fill.
+     *  We would have had a permutation if we had all the possible combinations of 3 elements in 3 spaces to fill.
+     *
+     *  Expected result: 3^6 = 729 records
+     */
+    private static void printProductCombinations() {
+        String[] products = {"A", "B", "C"};
+
+        List<String> combinationList = Lists.newArrayList();
+
+        int prodLength  = 3;
+        int a, b, c, d, e, f; // indexes for each character of the string to compose
+        for (a = 0; a < prodLength ; a++) {
+            for (b = 0; b < prodLength ; b++) {
+                for (c = 0; c < prodLength ; c++) {
+                    for (d = 0; d < prodLength ; d++) {
+                        for (e = 0; e < prodLength ; e++) {
+                            for (f = 0; f < prodLength ; f++) {
+                                String composedString = products[a] + products[b] + products[c] + products[d] + products[e] + products[f];
+                                combinationList.add(composedString);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        combinationList.forEach(System.out::println);
+        System.out.println("\nList length:" + combinationList.size());
+    }
+
+
 }
